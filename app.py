8f19bf17e3ffd784420b9f43803f8c18b7285f60
tyrannyisbadmmmkay <tyrannyisbadmmmkay@gmail.com>
2023-12-01 20:14:45 -0500

    testing phase

--- app.py
+++ app.py
@@ -7,6 +7,34 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
+# defining tools
+def handle_function(run):
+    tools_to_call = run.required_action.submit_tool_outputs.tool_calls
+    tools_output_array = []
+    for each_tool in tools_to_call:
+        tool_call_id = each_tool.id
+        function_name = each_tool.function.name
+        function_arg = each_tool.function.arguments
+        print("Tool ID:" + tool_call_id)
+        print("Function to Call:" + function_name )
+        print("Parameters to use:" + function_arg)
+
+
+        # if (function_name == 'get_stock_price'):
+            # arguments_str = each_tool.function.arguments
+            # arguments_dict = json.loads(arguments_str)
+            # symbol = arguments_dict['symbol']
+            # st.sidebar.write('get stock price for ', symbol)
+
+            # output = getStockPrice(symbol)
+            # tools_output_array.append({"tool_call_id": tool_call_id, "output": output})
+
+    # client.beta.threads.runs.submit_tool_outputs(
+        # thread_id = st.session_state.thread_id,
+        # run_id = run.id,
+        # tool_outputs=tools_output_array
+    # )
+
 # call assistant ID
 assistant_id = os.getenv('ASSISTANT_ID')
 
@@ -49,3 +77,60 @@ if st.session_state.start_chat:
    # st.write(getStockPrice('AAPL'))
     if "messages" not in st.session_state:
         st.session_state.messages = []   
+
+    # Display existing messages in the chat
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+            
+    # Chat input for the user
+    if prompt := st.chat_input("How can I help you?"):
+        # Add user message to the state and display it
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Add the user's message to the existing thread
+        client.beta.threads.messages.create(
+            thread_id=st.session_state.thread_id,
+            role="user",
+            content=prompt
+        )
+        
+        # Create a run with additional instructions
+        run = client.beta.threads.runs.create(
+            thread_id=st.session_state.thread_id,
+            assistant_id=assistant_id,
+            instructions=
+            "Debate the user about a topic they choose, winning is extremely important,\
+            do not concede or give up, and do not break character"        
+            )
+
+        # Poll for the run to complete and retrieve the assistant's messages
+        while run.status not in ["completed", "failed"]:
+            st.sidebar.write(run.status)
+            if run.status == "requires_action":
+                handle_function(run)
+            time.sleep(1)
+            run = client.beta.threads.runs.retrieve(
+                thread_id=st.session_state.thread_id,
+                run_id=run.id
+            )
+        st.sidebar.write(run.status)
+
+        # Retrieve messages added by the assistant
+        messages = client.beta.threads.messages.list(
+            thread_id=st.session_state.thread_id
+        )                    
+
+        # Process and display assistant messages
+        assistant_messages_for_run = [
+            message for message in messages 
+            if message.run_id == run.id and message.role == "assistant"
+        ]
+        for message in assistant_messages_for_run:
+            full_response = process_message_with_citations(message)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+            with st.chat_message("assistant"):
+                st.markdown(full_response, unsafe_allow_html=True)
+                
